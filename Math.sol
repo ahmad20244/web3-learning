3a4cb2785b53ad5dfcba84baa25d0682312994c3
ahmad20244 <136627576+ahmad20244@users.noreply.github.com>
2026-07-09 11:10:45 +0330

    Create Math.sol
    Add Math contract

---
+++ Math.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Math {
+
+    function add(uint a,uint b) public pure returns(uint){
+        return a+b;
+    }
+
+    function subtract(uint a,uint b) public pure returns(uint){
+        return a-b;
+    }
+}

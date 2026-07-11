04a5bb1e9dbc8df71aaf8be89873eb1112ebbe9e
ahmad20244 <136627576+ahmad20244@users.noreply.github.com>
2026-07-11 11:05:27 +0330

    Create Token.sol
    Add simple token contract

---
+++ Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Token {
+
+    string public name = "DemoToken";
+    string public symbol = "DMT";
+    uint public totalSupply = 1000000;
+
+}

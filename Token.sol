9ffa317d0445c86fe9097352461f9ce28e6c2ece
ahmad20244 <136627576+ahmad20244@users.noreply.github.com>
2026-07-18 14:25:17 +0330

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// Basic token example
 contract Token {
 
     string public name = "DemoToken";
     string public symbol = "DMT";
     uint public totalSupply = 1000000;
+
+    function getTokenInfo() public pure returns (string memory) {
+        return "DemoToken (DMT)";
+    }
 
 }

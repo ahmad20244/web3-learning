a0e3d880da28539e7d72c56019249c7eee98c34a
ahmad20244 <136627576+ahmad20244@users.noreply.github.com>
2026-07-20 10:17:20 +0330

    Update Calculator.sol

--- Calculator.sol
+++ Calculator.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.20;
 
 contract Calculator {
 
-    function add(uint a,uint b) public pure returns(uint){
-        return a+b;
+    function add(uint a, uint b) public pure returns(uint) {
+        return a + b;
     }
 
-    function sub(uint a,uint b) public pure returns(uint){
-        return a-b;
+    function subtract(uint a, uint b) public pure returns(uint) {
+        return a - b;
     }
+
+    function multiply(uint a, uint b) public pure returns(uint) {
+        return a * b;
+    }
+
 }

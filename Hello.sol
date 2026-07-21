11e946d2ca5deef7857451aad72d71ddea465d53
ahmad20244 <136627576+ahmad20244@users.noreply.github.com>
2026-07-21 16:53:12 +0330

    Update Hello.sol

--- Hello.sol
+++ Hello.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.20;
 
 contract Hello {
-    string public message = "Hello OPN";
-}
-function changeMessage(string memory _msg) public {
-    message = _msg; function getMessage  public view returns (string memory) {
-    return message;
-}
 
-// Simple smart contract
+    string public greeting = "Hello Web3";
+
+    function getGreeting() public view returns (string memory) {
+        return greeting;
+    }
+
+    function updateGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+}

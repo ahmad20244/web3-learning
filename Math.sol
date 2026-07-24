75e2c320e5c6b96f535a0afaa567122624fcbfc3
ahmad ghazavi <136627576+ahmad20244@users.noreply.github.com>
2026-07-24 17:04:34 +0330

    Update Math.sol

--- Math.sol
+++ Math.sol
@@ -3,11 +3,21 @@ pragma solidity ^0.8.20;
 
 contract Math {
 
-    function add(uint a,uint b) public pure returns(uint){
-        return a+b;
+    function add(uint a, uint b) public pure returns (uint) {
+        return a + b;
     }
 
-    function subtract(uint a,uint b) public pure returns(uint){
-        return a-b;
+    function subtract(uint a, uint b) public pure returns (uint) {
+        return a - b;
     }
+
+    function multiply(uint a, uint b) public pure returns (uint) {
+        return a * b;
+    }
+
+    function divide(uint a, uint b) public pure returns (uint) {
+        require(b != 0, "Cannot divide by zero");
+        return a / b;
+    }
+
 }

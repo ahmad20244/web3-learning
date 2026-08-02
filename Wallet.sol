8b2db0eb1f4d9f8daecaeba607842fbdd7d949c5
ahmad ghazavi <136627576+ahmad20244@users.noreply.github.com>
2026-08-02 17:05:40 +0330

    Add withdrawal validation

--- Wallet.sol
+++ Wallet.sol
@@ -2,16 +2,30 @@
 pragma solidity ^0.8.20;
 
 contract Wallet {
+    address public owner;
 
-address public owner; // Wallet owner
     constructor() {
         owner = msg.sender;
     }
 
-    function getOwner() public view returns(address) {
+    receive() external payable {}
+
+    function getOwner() public view returns (address) {
         return owner;
     }
-function version() public pure returns (string memory) {
-    return "1.0";
-}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(msg.sender == owner, "Not owner");
+        require(amount <= address(this).balance, "Insufficient balance");
+
+        payable(owner).transfer(amount);
+    }
+
+    function version() public pure returns (string memory) {
+        return "1.1";
+    }
 }
